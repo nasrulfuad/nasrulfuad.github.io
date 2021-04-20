a942d431b117a993c1eba301392846d4562b257d
Nasrul Fuad <nasrulfuad08@gmail.com>
2021-04-20 14:22:42 +0700

    add smooth scroll

--- src/assets/js/index.ts
+++ src/assets/js/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello There");
+window.onload = () => {
+  bootstrap();
+};
+
+function bootstrap() {
+  const navigationItems = document.querySelectorAll(".navigation__item");
+
+  navigationItems.forEach((navigationItem) => {
+    navigationItem.addEventListener("click", function (event) {
+      event.preventDefault();
+
+      const id = this.getAttribute("href");
+
+      document.querySelector(id).scrollIntoView();
+    });
+  });
+}
